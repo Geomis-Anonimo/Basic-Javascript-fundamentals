5bfe477efa80bc3cb94338794b5b762d53f9f219
Geomis-Anonimo <130933017+Geomis-Anonimo@users.noreply.github.com>
2024-03-29 14:29:57 -0300

    Add files via upload

---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,22 @@
+// par nome/valor
+const salutation = 'hey'; // contexto léxico 1
+
+function exec() {
+    const salutation = `what's up?`; //contexto léxico 2
+    return salutation;
+};
+
+// Objetos são grupos aninhados de pares nome/valor
+const client = {
+    name:'Anne',
+    age: 32,
+    weight: 70,
+    address: {
+        logradouro: 'very coll street', 
+        number: 123
+    }
+};
+
+console.log(salutation);
+console.log(exec());
+console.log(client);
---
+++ fundamentos/ternario.js
@@ -0,0 +1,5 @@
+const result = grade => grade >= 7 ? 'approved' : 'disapproved';
+
+console.log(result(7.1));
+console.log(result(6.7));
+
